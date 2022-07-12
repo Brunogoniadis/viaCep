4cf138f0d0d06126b9ac638ba47f812ee1ecfc27
Bruno Goniadis <56612714+Brunogoniadis@users.noreply.github.com>
2022-07-11 21:02:54 -0300

    Update nos Styles
    Update no container principal e nas Views restantes com StyleSheet

--- App.js
+++ App.js
@@ -1,21 +1,50 @@
-import React from "react";
+import React, {useState} from "react";
 import{View,Text, TextInput,TouchableOpacity, StyleSheet, SafeAreaView} from 'react-native';
 import api from './src/services/api';
 
 export default function App(){
+  const [cep, setCep] = useState('');
+  
   return(
     <SafeAreaView>
-      <Text>
-        Primeiro App, primeiro Commit
-      </Text>
+      <View style = {{alignItems: 'center'}}>
+        <Text style ={style.text}>Digite o cep Desejado</Text>
+        <TextInput
+        style={style.input}
+        placeholder = "Ex: 9879876"
+        value={cep}
+        onChangeText={(texto)=>setCep(texto)}
+        keyboardType="numeric"
+      />
+
+      </View>
+      
     </SafeAreaView>
 
   );
 }
 
-const style = StyleSheet.create.apply({
+const style = StyleSheet.create({
   container: {
     flex:1,
+  },
+  text:{
+    marginTop:25,
+    marginBottom:15,
+    fontSize:25,
+    fontWeight: 'bold',
+    color: 'black'
+
+  },
+  input:{
+    backgroundColor: 'white',
+    borderWidth: 1,
+    borderColor: '#black',
+    borderRadius: 5,
+    width: '90%',
+    padding: 10,
+    fontSize: 18,
+    tintColor: 'black'
   }
 
 })
